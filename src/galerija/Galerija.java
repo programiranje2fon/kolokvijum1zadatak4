e18e1eb6611adc018df24d95fe8ebab5bf06204e
bojantomic <tomicb@fon.rs>
2018-11-02 22:14:14 +0100

    Resen ceo prvi zadatak

--- src/galerija/Galerija.java
+++ src/galerija/Galerija.java
@@ -1,6 +1,9 @@
 package galerija;
 
+import java.util.GregorianCalendar;
+
 import eksponati.Eksponat;
+import eksponati.skulpture.Skulptura;
 
 public class Galerija {
 	
@@ -27,6 +30,7 @@ public class Galerija {
 		for (int i=0; i<eksponati.length; i++)
 			if (eksponati[i] == null) {
 				eksponati[i] = e;
+				e.setDatumPrijema(new GregorianCalendar());
 				return;
 			}
 
@@ -34,7 +38,42 @@ public class Galerija {
 	}
 	
 	public void uvediPopust(double procenatPopusta) {
+		GregorianCalendar trenutni = new GregorianCalendar();
 		
+		int godina = trenutni.get(GregorianCalendar.YEAR);
+		
+		for (int i=0;i<eksponati.length;i++)
+			if (eksponati[i]!=null && 
+				eksponati[i].getDatumPrijema().get(GregorianCalendar.YEAR) == godina-1) {
+				double novaCena = eksponati[i].getCena() * (100-procenatPopusta)/100;
+				eksponati[i].setCena(novaCena);
+			}
+	}
+	
+	public Skulptura[] vratiNajskuplje() {
+		Skulptura[] niz = new Skulptura[2];
+		
+		int max1Indeks = -1;
+
+		for (int i=0; i<eksponati.length; i++)
+			if (eksponati[i]!=null && eksponati[i] instanceof Skulptura &&
+					((Skulptura)(eksponati[i])).getMaterijal().equals("bronza"))
+				if (max1Indeks == -1 || eksponati[max1Indeks].getCena() < eksponati[i].getCena())
+					max1Indeks = i;
+		
+		int max2Indeks = -1;
+		
+		for (int i=0; i<eksponati.length; i++)
+			if (i!=max1Indeks && 
+					eksponati[i]!=null && eksponati[i] instanceof Skulptura &&
+					((Skulptura)(eksponati[i])).getMaterijal().equals("bronza"))
+				if (max2Indeks == -1 || eksponati[max2Indeks].getCena() < eksponati[i].getCena())
+					max2Indeks = i;
+		
+		if (max1Indeks != -1) niz[0] = (Skulptura)eksponati[max1Indeks];
+		if (max2Indeks != -1) niz[1] = (Skulptura)eksponati[max2Indeks];
+		
+		return niz;
 	}
 
 }
